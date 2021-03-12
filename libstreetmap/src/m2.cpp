4449d839692c20b5b4b19c341a7e6277de94f0a1
Alan <alanw.lau@mail.utoronto.ca>
2021-03-11 23:21:52 -0500

    Revert "added cpp file to declare map_load_path in another cpp other than main"
    This reverts commit 7b4074a21a4cf79cd750121ee36f6242951ab0f9.

--- libstreetmap/src/m2.cpp
+++ libstreetmap/src/m2.cpp
@@ -863,7 +863,7 @@ void draw_main_canvas (ezgl::renderer *g){
     ezgl::point2d small = world.bottom_left();
     ezgl::point2d large = world.top_right();
     
-    //std::cout << bounds.area/area << std::endl;
+    std::cout << bounds.area/area << std::endl;
 
     g -> set_color(243,243,239,255); 
     g -> fill_rectangle(world);
@@ -1067,7 +1067,7 @@ void search_entry_activate(GtkEntry* entry){
         street_segments[segments[i]].highlight = true;
         }
     
-    global_app->refresh_drawing();
+    app->refresh_drawing();
     street.clear();
     segments.clear();
     gtk_entry_set_text(entry, " ");
