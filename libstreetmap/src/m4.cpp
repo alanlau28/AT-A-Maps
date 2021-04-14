ac5ee940c7699ee6d79cf056efc2c19d60cdda2f
Alex <alexshiqi.tan@mail.utoronto.ca>
2021-04-14 18:32:09 -0400

    changed greedy algorithm to multi start, look for the fastest overall result
    Former-commit-id: 6149295d344cd317758ff3220e97c6bbb0c59205

--- libstreetmap/src/m4.cpp
+++ libstreetmap/src/m4.cpp
@@ -30,6 +30,7 @@ std::vector<CourierSubPath> travelingCourier(
     std::vector<IntersectionIdx> intersections_dest;
     std::vector<std::vector<std::vector<StreetSegmentIdx>>> all_paths;
     std::vector<CourierSubPath> subPath;
+    std::vector<CourierSubPath> bestPath;
             
     for(int i = 0; i < numDeliveries;i++){
         intersections_dest.push_back(deliveries[i].pickUp);
@@ -41,106 +42,151 @@ std::vector<CourierSubPath> travelingCourier(
     }
     //auto startTime = std::chrono::high_resolution_clock::now();
     all_paths = findAllPaths(intersections_dest,turn_penalty);
+
     //auto endTime = std::chrono::high_resolution_clock::now();
     //auto time = std::chrono::duration_cast<std::chrono::milliseconds>(endTime-startTime);
     //std::cout << time.count();
-    
-     //std::vector<std::vector<std::vector<StreetSegmentIdx>>> intersections_dest;
-    //std::vector<std::vector<StreetSegmentIdx>> answer;
-    std::vector<StreetSegmentIdx> currPath;
-    std::vector<double> travel_time_of_paths;
-    std::vector<bool> legal;
-    std::vector<bool> notVisited;
-    for (int i = 0; i < numDeliveries; i++){
-        legal.push_back(false);
-        notVisited.push_back(true);
-        notVisited.push_back(true);
-    }
-    double currMin = INT_MAX;
-    int depot_start, depot_end;
-    int prev;
-    int next;
-    double pathTime;
-    for(int i = 0; i<depots.size();i++){
-        int depot_index = (numDeliveries*2)+i;
-        for(int j = 0; j< all_paths[depot_index].size()-depots.size(); j+=2){
-            
-            pathTime = computePathTravelTime(all_paths[depot_index][j],turn_penalty);
-            
-            if(pathTime<currMin&& pathTime!=0){
-                currMin = pathTime;
-                depot_start = depot_index;
-                next = j;
-            }
-        }     
-    }
-    //answer.push_back(all_paths[depot_start][next]);
-    travel_time_of_paths.push_back(currMin);
-    
-    CourierSubPath path;
-    path.start_intersection = depots[depot_start-2*numDeliveries];
-    
-    path.end_intersection = deliveries[next/2].pickUp;
-    path.subpath = all_paths[depot_start][next];
-    subPath.push_back(path);
-    
-    notVisited[next]=false;   
-    legal[next/2]= true; 
-    
-    prev = next;
-    
-    for(int iteration = 0; iteration < 2*numDeliveries-1; iteration++){
-        currMin=INT_MAX;
-        if(prev%2==0){
-            path.start_intersection= deliveries[prev/2].pickUp;
-        }else if (prev%2!=0){
-            path.start_intersection= deliveries[prev/2].dropOff;
-        }
-        
-        for(int i = 0; i<(all_paths[prev].size()-depots.size()); i++){
-            if((legal[i/2]||i%2==0)&&notVisited[i]&&i!=prev){
-                pathTime = computePathTravelTime(all_paths[prev][i],turn_penalty);
-                if(pathTime<currMin && pathTime!=0){
-                     currMin = pathTime;
-                     next = i;             
-                }
-            }        
 
-        }
-        if(next%2==0){
-            legal[next/2]=true;
-            path.end_intersection = deliveries[next/2].pickUp;
-        }else if(next%2!=0){
-            legal[next/2]=false;
-            path.end_intersection = deliveries[next/2].dropOff;
-        }
-        notVisited[next]=false;
-        travel_time_of_paths.push_back(currMin);
-        //answer.push_back(all_paths[prev][next]);
-        path.subpath=all_paths[prev][next];
-        prev= next;
-        subPath.push_back(path);        
+    int depotit = 0;
+    while(depotit<depots.size()){
+        //std::vector<std::vector<std::vector<StreetSegmentIdx>>> intersections_dest;
+       //std::vector<std::vector<StreetSegmentIdx>> answer;
+       std::vector<CourierSubPath> currPath;
+       //std::vector<double> travel_time_of_paths;
+       std::vector<bool> legal;
+       std::vector<bool> notVisited;
+       for (int i = 0; i < numDeliveries; i++){
+           legal.push_back(false);
+           notVisited.push_back(true);
+           notVisited.push_back(true);
+       }
+       double currMin = INT_MAX;
+       double totalMin = INT_MAX;
+
+       int depot_start, depot_end;
+       int prev;
+       int next;
+       double pathTime, pathDistance;
+
+       //for(int i = 0; i<depots.size();i++){
+       //while(depotit<depots.size()){
+           double total = 0;
+           int depot_index = (numDeliveries*2)+depotit;
+           
+           for(int j = 0; j< all_paths[depot_index].size()-depots.size(); j+=2){
+               //pathTime = findEuclidianDistance(intersectionPosition[depots[i]],intersectionPosition[deliveries[j/2].pickUp]);
+               pathTime = computePathTravelTime(all_paths[depot_index][j],turn_penalty);
+               //std::cout<<pathTime<<std::endl;
+
+               if(pathTime<currMin&& pathTime!=0){
+                   currMin = pathTime;
+                   depot_start = depot_index;
+                   
+                   next = j;
+               }
+           }
+           if(currMin==INT_MAX){
+                //std::cout<<"ewfwefewefwfewfewfwf"<<std::endl;
+               depotit+=1;
+               continue;
+           }
+       //}
+       //answer.push_back(all_paths[depot_start][next]);
+       //travel_time_of_paths.push_back(currMin);
+           total+=currMin;
+       CourierSubPath path;
+       path.start_intersection = depots[depot_start-2*numDeliveries];
+
+       path.end_intersection = deliveries[next/2].pickUp;
+       path.subpath = all_paths[depot_start][next];
+       currPath.push_back(path);
+
+       notVisited[next]=false;   
+       legal[next/2]= true; 
+
+       prev = next;
+
+       for(int iteration = 0; iteration < 2*numDeliveries-1; iteration++){
+           currMin=INT_MAX;
+           if(prev%2==0){
+               path.start_intersection= deliveries[prev/2].pickUp;
+           }else if (prev%2!=0){
+               path.start_intersection= deliveries[prev/2].dropOff;
+           }
+
+           for(int i = 0; i<(all_paths[prev].size()-depots.size()); i++){
+               if((legal[i/2]||i%2==0)&&notVisited[i]&&i!=prev){
+                   pathTime = computePathTravelTime(all_paths[prev][i],turn_penalty);
+                   if(prev%2==0){
+                       if(i%2==0){
+                           pathDistance = findEuclidianDistance(intersectionPosition[deliveries[prev/2].pickUp], intersectionPosition[deliveries[i/2].pickUp]);
+                       }else if(i%2!=0){
+                           pathDistance = findEuclidianDistance(intersectionPosition[deliveries[prev/2].pickUp], intersectionPosition[deliveries[i/2].dropOff]);
+                       }
+                   }else if (prev%2!=0){
+                       if(i%2==0){
+                           pathDistance = findEuclidianDistance(intersectionPosition[deliveries[prev/2].dropOff], intersectionPosition[deliveries[i/2].pickUp]);
+                       }else if(i%2!=0){
+                           pathDistance = findEuclidianDistance(intersectionPosition[deliveries[prev/2].dropOff], intersectionPosition[deliveries[i/2].dropOff]);
+                       }
+                   }
+
+                   if((pathTime<currMin && pathTime!=0) ||(pathTime==0 && pathDistance == 0)){
+                        currMin = pathTime;
+                        next = i;             
+                   }
+               }        
+
+           }
+           if(next%2==0){
+               legal[next/2]=true;
+               path.end_intersection = deliveries[next/2].pickUp;
+           }else if(next%2!=0){
+               legal[next/2]=false;
+               path.end_intersection = deliveries[next/2].dropOff;
+           }
+           notVisited[next]=false;
+           //travel_time_of_paths.push_back(currMin);
+           //answer.push_back(all_paths[prev][next]);
+           path.subpath=all_paths[prev][next];
+           prev= next;
+           currPath.push_back(path);
+           total+=currMin;
+       }
+
+
+
+       currMin = INT_MAX;
+
+       path.start_intersection= deliveries[next/2].dropOff; 
+       for(int i = 2*numDeliveries; i < all_paths.size(); i++){
+           pathTime = computePathTravelTime(all_paths[next][i],turn_penalty);
+           //pathTime = findEuclidianDistance(intersectionPosition[deliveries[next/2].dropOff], intersectionPosition[depots[i-(2*numDeliveries)]]);
+           if(pathTime< currMin && pathTime!=0){
+               currMin = pathTime;
+               depot_end = i;
+           }
+       }
+       total+=currMin;
+       //answer.push_back(all_paths[next][depot_end]);
+       //travel_time_of_paths.push_back(currMin);
+       path.end_intersection = depots[depot_end-2*numDeliveries];
+       path.subpath = all_paths[next][depot_end];
+       currPath.push_back(path);
+       if(total<totalMin){
+           totalMin = total;
+           bestPath = currPath;
+           
+       }
+       
+       currPath.clear();
+       depotit+=1;
+
+    
     }
+
     
-    
-    
-    currMin = INT_MAX;
-   
-    path.start_intersection= deliveries[next/2].dropOff; 
-    for(int i = 2*numDeliveries; i < all_paths.size(); i++){
-        pathTime = computePathTravelTime(all_paths[next][i],turn_penalty);
-        if(pathTime< currMin){
-            currMin = pathTime;
-            depot_end = i;
-        }
-    }
-    //answer.push_back(all_paths[next][depot_end]);
-    travel_time_of_paths.push_back(currMin);
-    path.end_intersection = depots[depot_end-2*numDeliveries];
-    path.subpath = all_paths[next][depot_end];
-    subPath.push_back(path);
-    
-    return subPath;
+    return bestPath;
     
     
 }
